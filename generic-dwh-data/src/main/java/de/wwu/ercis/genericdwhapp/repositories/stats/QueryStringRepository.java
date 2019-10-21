cfa452dee1a85a6fa915080bc42aafed19d247f1
pedro.marangoni <pedro911@gmail.com>
2019-10-21 16:20:34 +0200

    added order by field id on query time repository

--- generic-dwh-data/src/main/java/de/wwu/ercis/genericdwhapp/repositories/stats/QueryStringRepository.java
+++ generic-dwh-data/src/main/java/de/wwu/ercis/genericdwhapp/repositories/stats/QueryStringRepository.java
@@ -3,6 +3,7 @@ package de.wwu.ercis.genericdwhapp.repositories.stats;
 import de.wwu.ercis.genericdwhapp.model.stats.QueryString;
 import de.wwu.ercis.genericdwhapp.repositories.NativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -17,6 +18,9 @@ public interface QueryStringRepository extends JpaRepository<QueryString, Long>,
 
     List<QueryString> findAllByOrderByQueryStringId();
 
+    @Query(value = "select * from query_string \n" +
+            "WHERE query_string.query_string_id IN (:queryIds) " +
+            "ORDER BY FIELD(query_string_id, :queryIds)", nativeQuery = true)
     List<QueryString> findAllByQueryStringIdIn(List<Long> queryIds);
 
 }
